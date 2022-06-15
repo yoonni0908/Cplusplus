da23ea32aaed75bce55a0f4fbb2f40c7d63b7212
yoonni0908 <100178567+yoonni0908@users.noreply.github.com>
2022-06-15 11:28:26 +0900

    Add files via upload

---
+++ week9.상속_ticket/AdvancedTicket.cpp
@@ -0,0 +1,32 @@
+#include "AdvancedTicket.h"
+#include <iostream>
+using namespace std;
+
+AdvancedTicket::AdvancedTicket(const double& price, const int& advanceDays)
+	:Ticket(price), advanceDays(advanceDays)
+{
+}
+
+double AdvancedTicket::getPrice() const
+{
+	if (advanceDays >= 30)
+		return price * 0.5;
+	else if (advanceDays >= 20)
+		return price * 0.7;
+	else if (advanceDays >= 10)
+		return price * 0.9;
+	else
+		return price;
+}
+
+int AdvancedTicket::getAdvanceDays() const
+{
+	return advanceDays;
+}
+
+void AdvancedTicket::show() const
+{
+	Ticket::show();
+	cout << "사전예약일 : " << getAdvanceDays() << endl;
+	cout << "지불금액 : " << getPrice() << endl;
+}
---
+++ week9.상속_ticket/Ticket.cpp
@@ -0,0 +1,28 @@
+#include "Ticket.h"
+#include <iostream>
+using namespace std;
+
+Ticket::Ticket(const double& price):number(++count), price(price)
+{
+}
+
+int Ticket::getNumber() const
+{
+	return number;
+}
+
+double Ticket::getPrice() const
+{
+	return price;
+}
+
+void Ticket::setPrice(const double& price)
+{
+	this->price = price;
+}
+
+void Ticket::show() const
+{
+	cout << "티켓번호 : " << getNumber() << endl;
+	cout << "가격정보 : " << getPrice() << endl;
+}
